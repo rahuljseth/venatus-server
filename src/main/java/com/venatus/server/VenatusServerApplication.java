7da4d1b5f2d5a3bc94ad7266b9370a80649f05ab
BishalDeyMCA <bdey4002.github@gmail.com>
2025-09-12 18:46:48 +0530

    Initial Spring Boot Project Setup

---
+++ src/main/java/com/venatus/server/VenatusServerApplication.java
@@ -0,0 +1,13 @@
+package com.venatus.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenatusServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenatusServerApplication.class, args);
+	}
+
+}
